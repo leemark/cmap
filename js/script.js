8de6a8050504e21ebaa22a465809d3b5e4816b6b
leemark <leemark@gmail.com>
2014-01-23 19:04:02 -0700

    update zoom level

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var map = L.map('cmap', {drawControl: true}).setView([38.8486555, -104.824], 20);
+var map = L.map('cmap', {drawControl: true}).setView([38.8486555, -104.824], 18);
 var osmTiles = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 });
