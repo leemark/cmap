af75be42c1b2d3db52478ea05ec441d4bba5e36e
leemark <leemark@gmail.com>
2014-01-27 22:40:02 -0700

    adding ajax plugin

--- js/script.js
+++ js/script.js
@@ -2,4 +2,6 @@ var map = L.map('cmap', {drawControl: true}).setView([38.8486555, -104.824], 18)
 var osmTiles = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 });
-osmTiles.addTo(map)
+osmTiles.addTo(map);
+var geojsonLayer = new L.GeoJSON.AJAX("data/ccgeo.json");
+geojsonLayer.addTo(map);
