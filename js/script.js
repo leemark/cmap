f1b70f66979c28ad9ecdbc4fafdb55151c7ab4bf
leemark <leemark@gmail.com>
2014-08-18 15:04:40 -0600

    added full-screen control

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 (function(L){
-    var map = L.map('cmap', {drawControl: true}).setView([38.8486555, -104.824], 18),
+    var map = L.map('cmap', {drawControl: true, fullscreenControl: true}).setView([38.8486555, -104.824], 18),
         osmTiles = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     }),
